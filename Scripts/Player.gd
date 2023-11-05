f880632517e65fd18b9fd797417f88622e4ab12d
Pacha <cardosorodrigoa@gmail.com>
2023-11-05 01:20:19 -0300

    deleted:    .import/hospital-tile-2_0000s_0000_Capa-13.png-d6eb315cbfac5fb039e4ffdfbac17441.stex 	deleted:    .import/hospital-tile-2_0000s_0001_Capa-12.png-175fbbbf66a37746c3618824fecca2a5.stex 	deleted:    .import/hospital-tile-2_0000s_0002_piso-copia-5.png-b8ce68e5fe264ae3923214813d9faf34.stex 	deleted:    Assets/Arte/hospital capas/hospital-tile-2_0000s_0000_Capa-13.png 	deleted:    Assets/Arte/hospital capas/hospital-tile-2_0000s_0000_Capa-13.png.import 	deleted:    Assets/Arte/hospital capas/hospital-tile-2_0000s_0001_Capa-12.png 	deleted:    Assets/Arte/hospital capas/hospital-tile-2_0000s_0001_Capa-12.png.import 	deleted:    Assets/Arte/hospital capas/hospital-tile-2_0000s_0002_piso-copia-5.png 	deleted:    Assets/Arte/hospital capas/hospital-tile-2_0000s_0002_piso-copia-5.png.import 	modified:   Scenes/Main.tscn 	modified:   Scenes/Menu.tscn 	modified:   Scenes/Player.tscn 	modified:   Scripts/Lantern.gd 	modified:   Scripts/Player.gd 	.import/hospital-tile-2_0000_Capa-13.png-0b2a36bbbbdd18b5cf6ec2cd394b059f.md5 	.import/hospital-tile-2_0000_Capa-13.png-0b2a36bbbbdd18b5cf6ec2cd394b059f.stex 	.import/hospital-tile-2_0001_Capa-12.png-fbced3e02c8938745275329267daa701.md5 	.import/hospital-tile-2_0001_Capa-12.png-fbced3e02c8938745275329267daa701.stex 	.import/hospital-tile-2_0002_piso-copia-5.png-02410cf43c54f10771800e1bbb38890e.md5 	.import/hospital-tile-2_0002_piso-copia-5.png-02410cf43c54f10771800e1bbb38890e.stex 	.import/hospital-tile-2_0003_Capa-11.png-73d20416f204dcb6650ecb703ffcc208.md5 	.import/hospital-tile-2_0003_Capa-11.png-73d20416f204dcb6650ecb703ffcc208.stex 	.import/menu_bkgr.png-ac809a36d5edc82a8151e52ad733cbf7.md5 	.import/menu_bkgr.png-ac809a36d5edc82a8151e52ad733cbf7.stex 	.import/whaaaat.png-0c44d5500d6e202ea44dea822a42c657.md5 	.import/whaaaat.png-0c44d5500d6e202ea44dea822a42c657.stex 	Assets/Arte/hospital capas/hospital-tile-2_0000_Capa-13.png 	Assets/Arte/hospital capas/hospital-tile-2_0000_Capa-13.png.import 	Assets/Arte/hospital capas/hospital-tile-2_0001_Capa-12.png 	Assets/Arte/hospital capas/hospital-tile-2_0001_Capa-12.png.import 	Assets/Arte/hospital capas/hospital-tile-2_0002_piso-copia-5.png 	Assets/Arte/hospital capas/hospital-tile-2_0002_piso-copia-5.png.import 	Assets/Arte/hospital capas/hospital-tile-2_0003_Capa-11.png 	Assets/Arte/hospital capas/hospital-tile-2_0003_Capa-11.png.import 	Assets/Arte/menu_bkgr.png 	Assets/Arte/menu_bkgr.png.import 	Assets/Arte/whaaaat.png 	Assets/Arte/whaaaat.png.import

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,13 +2,18 @@ extends KinematicBody2D
 
 onready var Luz := $Light2D
 onready var Temp := $Timer
+onready var Msje := $PopUp
+export var count :=100
 
-var speed = 300
+func _ready():
+	Msje.visible = false
+
+var speed = 150
 
 var target_position = Vector2.ZERO
 var is_moving = false
 
-func _process(delta):
+func _process(_delta):
 #	if is_moving:
 #		var direction = (target_position - position).normalized()
 #		direction = move_and_slide(direction * speed)
@@ -17,7 +22,13 @@ func _process(delta):
 #			is_moving = false
 #
 #		position += Vector2(speed * delta, speed * delta).normalized()
-#
+	count -=1
+	if count == 0:
+		Msje.visible = true
+		yield(get_tree().create_timer(2,5),"timeout")
+		Msje.visible = false
+		count=100
+
 	var cuadrant = calculateMouseQuadrant()
 	
 	if cuadrant != null:
@@ -26,13 +37,13 @@ func _process(delta):
 func calculateMouseQuadrant():
 	var mousePos = get_local_mouse_position()
 	if mousePos.x > 0 && mousePos.y > 0: 
-		return 0
+		return 3
 	if mousePos.x > 0 && mousePos.y < 0: 
 		return 1
 	if mousePos.x < 0 && mousePos.y > 0: 
 		return 2
 	if mousePos.x < 0 && mousePos.y < 0: 
-		return 3
+		return 0
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
